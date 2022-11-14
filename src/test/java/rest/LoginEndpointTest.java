b0643c0ee2e5f9134582c36377b9a43e6d996044
MuneebAshraf <Munne_122@hotmail.com>
2022-11-14 13:16:37 +0100

    remove https

--- src/test/java/rest/LoginEndpointTest.java
+++ src/test/java/rest/LoginEndpointTest.java
@@ -26,7 +26,7 @@ import utils.EMF_Creator;
 public class LoginEndpointTest {
 
     private static final int SERVER_PORT = 7777;
-    private static final String SERVER_URL = "https://localhost/weatherNCat/api";
+    private static final String SERVER_URL = "http://localhost/weatherNCat/api";
 
     static final URI BASE_URI = UriBuilder.fromUri(SERVER_URL).port(SERVER_PORT).build();
     private static HttpServer httpServer;
