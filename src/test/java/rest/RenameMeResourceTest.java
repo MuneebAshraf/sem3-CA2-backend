812e800d2569e9bd9db3ce0da07e1a00edef8236
MuneebAshraf <Munne_122@hotmail.com>
2022-11-14 12:36:27 +0100

    Update RenameMeResourceTest.java

--- src/test/java/rest/RenameMeResourceTest.java
+++ src/test/java/rest/RenameMeResourceTest.java
@@ -26,7 +26,7 @@ import org.junit.jupiter.api.Test;
 public class RenameMeResourceTest {
 
     private static final int SERVER_PORT = 7777;
-    private static final String SERVER_URL = "http://localhost/api";
+    private static final String SERVER_URL = "http://localhost/weatherNCat/api";
     private static RenameMe r1, r2;
 
     static final URI BASE_URI = UriBuilder.fromUri(SERVER_URL).port(SERVER_PORT).build();
